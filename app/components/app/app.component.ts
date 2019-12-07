b1d611abaf87c7a6d94c4858d21c56a04de3a2f3
Denys Rakov <bobsixpackbob@gmail.com>
2019-12-07 16:33:04 +0200

    Temp solution to fix back navigation

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component, AfterViewInit } from "@angular/core";
+import { Component, AfterViewInit, NgZone } from "@angular/core";
 import { topmost } from "tns-core-modules/ui/frame/frame";
 import { device } from "tns-core-modules/platform/platform";
 import { isAndroid, isIOS } from "tns-core-modules/ui/page/page";
 import { Color } from "tns-core-modules/ui/core/view";
-import { android as androidObj, AndroidApplication } from "tns-core-modules/application/application";
+import { android as androidListener, AndroidApplication } from "tns-core-modules/application/application";
 
 import { GoogleFirebaseService } from "../../services/google-firebase/google-firebase.service";
 import { AppThemeService } from "../../services/app-theme/app-theme.service";
+import { RouterExtensions } from "nativescript-angular/router";
+import { filter } from "rxjs/operators";
+import { NavigationEnd, NavigationStart } from "@angular/router";
 
 if (isAndroid && parseInt(device.sdkVersion, 10) >= 21) {
-    androidObj.on(AndroidApplication.activityStartedEvent, () => {
-        const window = androidObj.startActivity.getWindow();
+    androidListener.on(AndroidApplication.activityStartedEvent, () => {
+        const window = androidListener.startActivity.getWindow();
         window.addFlags((android.view.WindowManager.LayoutParams as any).FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags((android.view.WindowManager.LayoutParams as any).FLAG_TRANSLUCENT_STATUS);
         window.setNavigationBarColor(new Color("black").android);
@@ -25,12 +28,51 @@ if (isAndroid && parseInt(device.sdkVersion, 10) >= 21) {
 })
 export class AppComponent implements AfterViewInit  {
     public enableFpsMeter = false;
+    private previousUrl: string;
 
     constructor (
         private AppTheme: AppThemeService,
-        private GoogleFirebase: GoogleFirebaseService
+        private GoogleFirebase: GoogleFirebaseService,
+        private routerExtensions: RouterExtensions,
+        private zone: NgZone
     ) {
         this.GoogleFirebase.init();
+    }
+
+    public ngOnInit () {
+        // TODO Temp solution to fix "java.lang.IllegalStateException: The specified child already has a parent." error until BottomNavigation component is fixed
+        this.routerExtensions.router.events.pipe(
+            filter((event) => event instanceof NavigationStart)
+        )
+        .subscribe((e: any) => {
+            this.previousUrl = this.routerExtensions.router.url;
+        });
+
+        androidListener.on(AndroidApplication.activityBackPressedEvent, (args: any) => {
+            let canGoBack = false;
+            try {
+                canGoBack = this.routerExtensions.canGoBack();
+            }
+            catch (err) {
+                canGoBack = false;
+            }
+
+            if (canGoBack) {
+                args.cancel = true;
+                this.zone.run(() => {
+                    this.routerExtensions.navigate([this.previousUrl], {
+                        transition: {
+                            duration: 500,
+                            curve: "ease",
+                            name: "slideRight"
+                        }
+                    });
+                });
+            }
+            else {
+                args.cancel = false;
+            }
+        });
     }
 
     public ngAfterViewInit () {
